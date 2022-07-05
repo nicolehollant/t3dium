33aa4fed4b9f42ef4277a71fbced52d3145c5310
Cole Hollant <coleghollant@gmail.com>
2022-07-05 13:47:36 -0400

    ugh

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,7 +20,7 @@ export default NextAuth({
           TwitterProvider({
             clientId: process.env.TWITTER_ID ?? '',
             clientSecret: process.env.TWITTER_SECRET ?? '',
-            version: '1.0',
+            version: '2.0',
           }),
           EmailProvider({
             server: {
