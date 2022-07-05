64be0293b0418e2de44be7cb4336c60941ceec93
Cole Hollant <coleghollant@gmail.com>
2022-07-05 09:20:53 -0400

    sign in button shown by default

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,19 +1,21 @@
-import Link from "next/link";
-import { FC } from "react";
-import LoginButton from "./login-button";
+import Link from 'next/link'
+import { FC } from 'react'
+import LoginButton from './login-button'
 
-const AppHeader: FC<{ children?: JSX.Element | JSX.Element[] }> = ({children}) => {
-  return <>
-    <header className="flex justify-between px-4 items-center gap-4 py-2 border-b border-b-gray-400 bg-emerald-200/60 backdrop-filter backdrop-blur sticky top-0 shadow">
-      <h1 className="font-extrabold text-center text-3xl">
-        <Link href='/'>
-          📚 T3dium
-        </Link>
-      </h1>
-      <LoginButton></LoginButton>
-    </header>
-    {children}
-  </>
+const AppHeader: FC<{ children?: JSX.Element | JSX.Element[] }> = ({
+  children,
+}) => {
+  return (
+    <>
+      <header className="sticky top-0 flex items-center justify-between gap-4 border-b border-b-gray-400 bg-emerald-200/60 px-4 py-2 shadow backdrop-blur backdrop-filter">
+        <h1 className="text-center text-3xl font-extrabold">
+          <Link href="/">📚 T3dium</Link>
+        </h1>
+        <LoginButton showSignInButton={true}></LoginButton>
+      </header>
+      {children}
+    </>
+  )
 }
 
 export default AppHeader
