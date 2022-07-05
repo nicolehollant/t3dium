00174b525ee1fb722f830ebfa39a76a8a94f7c8e
Cole Hollant <coleghollant@gmail.com>
2022-07-05 12:21:45 -0400

    revert to oauth1 for twitter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,7 +20,6 @@ export default NextAuth({
           TwitterProvider({
             clientId: process.env.TWITTER_ID ?? '',
             clientSecret: process.env.TWITTER_SECRET ?? '',
-            version: '2.0',
           }),
           EmailProvider({
             server: {
